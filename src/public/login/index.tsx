a2ec3e3c9e28c7911c6068c45958b5869b3b6539
Johan <johan@macair2020>
2020-08-14 00:30:51 +0200

    added link on login page

--- src/public/login/index.tsx
+++ src/public/login/index.tsx
@@ -1,3 +1,12 @@
 import React from 'react';
-
-export default () => <>Login</>;
+import { UI } from '@nexys/material-components';
+export default () => (
+  <>
+    <h2>Login</h2>
+    <ul>
+      <li>
+        <UI.Link to={'/app'}>to app</UI.Link>
+      </li>
+    </ul>
+  </>
+);
